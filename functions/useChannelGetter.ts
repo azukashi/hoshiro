3a2d044a6e2645bd2c86726978993c6adb54cf37
Azka <falcxxdev@pm.me>
2023-11-26 09:10:26 +0000

    feat(func): add useChannelGetter function

---
+++ functions/useChannelGetter.ts
@@ -0,0 +1,9 @@
+import { Innertube } from 'youtubei.js';
+
+// @ts-ignore
+export const useChannelGetter = async (url: string) => {
+    const innerTube = await Innertube.create();
+    const resolved = await innerTube.resolveURL(url);
+    const channelId = await resolved.payload.browseId;
+    return await innerTube.getChannel(channelId);
+};
