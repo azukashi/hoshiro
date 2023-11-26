08545f2ea13b45c650fe03b4856fd2a70ebb7f77
Azka <falcxxdev@pm.me>
2023-11-26 09:08:24 +0000

    feat(ts): migrate app to typescript

---
+++ app.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import routes from './routes';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+if (!process.env.DATABASE_URI) throw new Error('DATABASE_URI is needed! Check again your .env file');
+
+app.use(express.json());
+app.listen(port, () => {
+    console.log(`[INFO] Express server has been started! (Live at port ${port})`);
+});
+
+mongoose.connect(process.env.DATABASE_URI as string, { retryWrites: true }).then(() => {
+    console.log('[INFO] App connected to MongoDB Atlas!');
+    app.use('/api', routes);
+});
