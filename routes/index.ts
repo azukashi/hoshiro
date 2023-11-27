d94ba980841ea7c48fe6c5bd3cbf3eff76566abd
Azka <falcxxdev@pm.me>
2023-11-27 16:03:28 +0000

    refactor(routes): make the route dynamic with the regions

--- routes/index.ts
+++ routes/index.ts
@@ -1,39 +1,39 @@
 import express, { Router } from 'express';
+import { useYouTubeData } from '../functions/useYouTubeData';
 import Indonesia from '../models/Indonesia';
-import { useChannelGetter } from '../functions/useChannelGetter';
-import { YTNodes } from 'youtubei.js';
+import Malaysia from '../models/Malaysia';
+import Singapore from '../models/Singapore';
+import Vietnam from '../models/Vietnam';
 
 const router: Router = express.Router();
+const regions = [
+    { code: 'id', db: Indonesia },
+    { code: 'my', db: Malaysia },
+    { code: 'sg', db: Singapore },
+    { code: 'vn', db: Vietnam },
+];
 
-router.get('/id', async (req, res) => {
-    const id = await Indonesia.find();
-    res.send(id);
-});
-
-router.get('/id/active', async (req, res) => {
-    const id = await Indonesia.find({ status: 'ACTIVE' });
-    res.send(id);
-});
-
-router.get('/id/graduated', async (req, res) => {
-    const id = await Indonesia.find({ status: 'GRADUATED' });
-    res.send(id);
-});
-
-router.get('/id/:handle', async (req, res) => {
-    const id = await Indonesia.findOne({ handle: req.params.handle });
-    if (!id) return res.status(404).send({ error: 'Queried handle not found!' });
-    const data = await useChannelGetter(`https://youtube.com/${req.params.handle}`);
-    const header = data.header?.as(YTNodes.C4TabbedHeader);
-    const filterred = {
-        picture: data.metadata.avatar,
-        banner: header?.banner,
-        subscribers: header?.subscribers?.text,
-        videos: header?.videos_count?.text,
-        is_verified: header?.author.is_verified,
-        is_membership: !header?.sponsor_button?.is_disabled,
-    };
-    res.send({ ...id.toJSON(), ...filterred });
+regions.forEach((region) => {
+    router.get(`/${region.code}`, async (req, res) => {
+        const data = await region.db.find();
+        res.send(data);
+    });
+    router.get(`/${region.code}/active`, async (req, res) => {
+        const data = await region.db.find({ status: 'ACTIVE' });
+        res.send(data);
+    });
+    router.get(`/${region.code}/graduated`, async (req, res) => {
+        const data = await region.db.find({ status: 'GRADUATED' });
+        res.send(data);
+    });
+    router.get(`/${region.code}/:handle`, async (req, res) => {
+        let params: any = { handle: req.params.handle };
+        if (region.code == 'vn') params = { ch_id: req.params.handle };
+        let data = await region.db.findOne(params);
+        if (!data) return res.status(404).send({ error: 'Queried handle not found!' });
+        const filterred = await useYouTubeData(req.params.handle as string, region.code == 'vn');
+        res.send({ ...data.toJSON(), ...filterred });
+    });
 });
 
 export default router;
