325e93044e3269755f18cad7c3af7d738f5f29c4
Azka <falcxxdev@pm.me>
2023-12-01 13:20:21 +0000

    feat(func): add useChannelAbout function

--- functions/useChannelGetter.ts
+++ functions/useChannelGetter.ts
@@ -7,3 +7,10 @@ export const useChannelGetter = async (url: string) => {
     const channelId = await resolved.payload.browseId;
     return await innerTube.getChannel(channelId);
 };
+
+export const useChannelAbout = async (url: string) => {
+    const innerTube = await Innertube.create();
+    const resolved = await innerTube.resolveURL(url);
+    const channelId = await resolved.payload.browseId;
+    return (await innerTube.getChannel(channelId)).getAbout();
+};
--- functions/useYouTubeData.ts
+++ functions/useYouTubeData.ts
@@ -1,4 +1,4 @@
-import { useChannelGetter } from './useChannelGetter';
+import { useChannelGetter, useChannelAbout } from './useChannelGetter';
 import { YTNodes } from 'youtubei.js';
 
 // @ts-ignore
@@ -6,14 +6,24 @@ export const useYouTubeData = async (handle: string, useId?: bool) => {
     let url = `https://youtube.com/${handle}`;
     if (useId) url = `https://youtube.com/channel/${handle}`;
     const data = await useChannelGetter(url);
-    const header = data.header?.as(YTNodes.C4TabbedHeader);
+    const about = await useChannelAbout(url);
+    const datahead = data.header?.as(YTNodes.C4TabbedHeader);
+    const abouthead = about?.as(YTNodes.AboutChannel);
     const filterred = {
-        picture: data.metadata.avatar,
-        banner: header?.banner,
-        subscribers: header?.subscribers?.text,
-        videos: header?.videos_count?.text,
-        is_verified: header?.author.is_verified,
-        is_membership: !header?.sponsor_button?.is_disabled,
+        picture: data?.metadata.avatar,
+        banner: datahead?.banner,
+        subscribers: datahead?.subscribers?.text,
+        videos: datahead?.videos_count?.text,
+        is_verified: datahead?.author.is_verified,
+        is_membership: !datahead?.sponsor_button?.is_disabled,
     };
-    return filterred;
+    const filterredAbout = {
+        views_count: abouthead.metadata?.view_count,
+        joined_date: abouthead.metadata?.joined_date?.text,
+        country: abouthead.metadata?.country,
+        description: abouthead.metadata?.description,
+        links: abouthead.metadata?.links,
+    };
+
+    return { ...filterred, ...filterredAbout };
 };
