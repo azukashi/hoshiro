fc237648c8b2f6638a5696967ca1db20012cae4a
Azka <falcxxdev@pm.me>
2023-12-30 06:32:55 +0000

    feat(const): add regions to constants

---
+++ constants/regions.ts
@@ -0,0 +1,11 @@
+import Indonesia from '../models/Indonesia';
+import Malaysia from '../models/Malaysia';
+import Singapore from '../models/Singapore';
+import Vietnam from '../models/Vietnam';
+
+export const regions = [
+    { code: 'id', db: Indonesia },
+    { code: 'my', db: Malaysia },
+    { code: 'sg', db: Singapore },
+    { code: 'vn', db: Vietnam },
+];
