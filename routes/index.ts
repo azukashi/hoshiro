3cd0e66c098bce9768fa450f6133f42864c2871f
Azka <falcxxdev@pm.me>
2023-12-31 21:20:10 +0000

    feat(routes): add POST & PATCH endpoints
    POST to create new entry and PATCH to update existing entry

--- routes/index.ts
+++ routes/index.ts
@@ -1,5 +1,8 @@
 // @ts-nocheck
 import express, { Router } from 'express';
+import { authenticateJWT } from '../middleware/auth';
+import { Request } from 'express-jwt';
+import { Response } from 'express';
 import { useYTData } from '../functions/useYTData';
 import { apiRoute } from './info/api';
 import { registerRoute } from './auth/register';
@@ -35,6 +38,59 @@ regions.forEach((region) => {
         const filterred = await useYTData(req.params.handle as string, region.code == 'vn');
         res.send({ ...data.toJSON(), ...filterred });
     });
+
+    // Protected routes
+    router.post(`/${region.code}`, [
+        authenticateJWT(),
+        async (req: Request, res: Response) => {
+            let { name, personality, birthdate, group, status, handle } = req.body;
+            let contributors = req.auth;
+            if (!name) return res.status(401).json({ message: 'Name is required!' });
+            if (!personality) personality = '-';
+            if (!birthdate) birthdate = '-';
+            if (!group) group = '-';
+            if (!status) return res.status(401).json({ message: 'Status is required!' });
+            if (!handle) return res.status(401).json({ message: 'YouTube handle is required!' });
+            const data = new region.db({
+                name: name,
+                personality: personality,
+                birthdate: birthdate,
+                group: group,
+                status: status,
+                handle: handle,
+                contributors: [contributors],
+            });
+            await data.save();
+            res.send(data);
+        },
+    ]);
+    router.patch(`/${region.code}/:handle`, [
+        authenticateJWT(),
+        async (req: Request, res: Response) => {
+            let { name, personality, birthdate, group, status, handle } = req.body;
+            let params = { handle: req.params.handle };
+            let contributors = req.auth;
+
+            try {
+                const data = await region.db.findOne(params);
+
+                if (name) data.name = name;
+                if (personality) data.personality = personality;
+                if (birthdate) data.birthdate = birthdate;
+                if (group) data.group = group;
+                if (status) data.status = status;
+                if (handle) data.handle = handle;
+                if (!data.contributors.find(({ username }) => username === contributors?.username))
+                    data.contributors.push(contributors);
+
+                await data.save();
+                res.send(data);
+            } catch (err) {
+                res.status(404).json({ message: 'Handle does not exist!' });
+                console.log(err);
+            }
+        },
+    ]);
 });
 
 export default router;
