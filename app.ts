80b3e857e589fb02e0cb7896415e168e7ee095c0
Azka <falcxxdev@pm.me>
2023-12-31 03:30:46 +0000

    chore(err): throw an error if env is empty

--- app.ts
+++ app.ts
@@ -8,7 +8,8 @@ import { authErrorHandler } from './middleware/autherror';
 const app = express();
 const port = process.env.PORT || 3000;
 
-if (!process.env.DATABASE_URI) throw new Error('DATABASE_URI is needed! Check again your .env file');
+if (!process.env.DATABASE_URI) throw new Error('DATABASE_URI is required! Please check your .env file');
+if (!process.env.SECRET_SESSION) throw new Error('SECRET_SESSION is required! Please check your .env file');
 
 mongoose.connect(process.env.DATABASE_URI as string, { retryWrites: true }).then(() => {
     console.log('[INFO] App connected to MongoDB Atlas!');
