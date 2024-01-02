1b3bb0743033d9549fa7d517d855e284639acc28
Azka <falcxxdev@pm.me>
2024-01-02 09:53:39 +0000

    fix(routes): send previously /api message to root

--- routes/info/root.ts
+++ routes/info/root.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
+import { name, version } from '../../package.json';
 
 export const root = (req: Request, res: Response) => {
-    res.send({ message: 'Hello, consider using /api as a root instead of this root path.' });
+    res.send({ _APPNAME: name, message: 'Hello!', version: version });
 };
