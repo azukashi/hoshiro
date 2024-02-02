6ca057a892132ed88470b60a865f0e25692c769e
Azka <falcxxdev@pm.me>
2024-02-02 16:48:49 +0000

    feat(cors): use cors as middleware

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 import routes from './routes';
 import { authErrorHandler } from './middleware/autherror';
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('short'));
+app.use(cors());
 app.use('/', routes);
 app.use(authErrorHandler);
 
