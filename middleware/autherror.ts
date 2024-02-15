01a16d0721e76f3b803510f8ffae48f958e8b004
Azka <falcxxdev@pm.me>
2024-02-15 09:29:55 +0000

    refactor(rm): remove unused files

--- middleware/autherror.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Response, Request } from 'express';
-
-export const authErrorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    if (err.name === 'UnauthorizedError') {
-        res.status(401).send({ message: 'Unauthorized access' });
-    } else {
-        next(err);
-    }
-};
--- routes/info/root.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import { name, version } from '../../package.json';
-
-export const root = (req: Request, res: Response) => {
-    res.send({ _APPNAME: name, message: 'Hello!', version: version });
-};
