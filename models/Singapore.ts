18f033fa8e232bffa4c322130b52d005bdae1449
Azka <falcxxdev@pm.me>
2024-02-15 10:01:21 +0000

    fix(schema): disable version key auto-generations

--- models/Singapore.ts
+++ models/Singapore.ts
@@ -1,13 +1,18 @@
 import mongoose from 'mongoose';
 
-const schema = new mongoose.Schema({
-    name: String,
-    personality: String,
-    birthdate: String,
-    group: String,
-    status: String,
-    handle: String,
-    contributors: Array,
-});
+const schema = new mongoose.Schema(
+    {
+        name: String,
+        personality: String,
+        birthdate: String,
+        group: String,
+        status: String,
+        handle: String,
+        contributors: Array,
+    },
+    {
+        versionKey: false,
+    }
+);
 
 export default mongoose.model('Singapore', schema);
