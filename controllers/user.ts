d2f7ff2239a4ffa8ac98f3fc5471d4b080cf70e6
Azka <falcxxdev@pm.me>
2024-02-15 10:05:45 +0000

    feat(users): add users controller

---
+++ controllers/user.ts
@@ -0,0 +1,33 @@
+import User from '../models/auth/User';
+
+export const index = async (req, res) => {
+    const { full } = req.query;
+    const users = await (full ? User.find() : User.find().simple());
+
+    res.json(users);
+};
+
+export const store = async (req, res, next) => {
+    try {
+        const { username, email, password, picture } = req.body;
+
+        const user = await User.create({ username, email, password, picture });
+
+        res.status(201).send({ user: user.toJSON() });
+    } catch (err) {
+        next(err);
+    }
+};
+
+export const update = async (req, res, next) => {
+    try {
+        let { user } = req.data;
+        const { username, email, picture } = req.body;
+
+        user = await User.findByIdAndUpdate(user.id, { username, email, picture }, { new: true });
+
+        res.json({ user: user.toJSON() });
+    } catch (err) {
+        next(err);
+    }
+};
