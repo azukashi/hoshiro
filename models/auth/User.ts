f8ff3d2b899a7afa92fbe4aa8930a119960b0395
Azka <falcxxdev@pm.me>
2024-02-15 09:47:59 +0000

    refactor(schema): rewrite user model with methods

--- models/auth/User.ts
+++ models/auth/User.ts
@@ -1,15 +1,65 @@
+import crypto from 'crypto';
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
 
 export interface UserDocument {
     username: string;
+    email: string;
     password: string;
+    discordId: string;
+    picture: string;
     token?: string;
 }
 
-const userSchema = new mongoose.Schema<UserDocument>({
-    username: String,
-    password: String,
-    token: String,
+const userSchema = new mongoose.Schema<UserDocument>(
+    {
+        username: { type: String, required: true },
+        email: { type: String, unique: true },
+        password: { type: String, select: false },
+        discordId: { type: String },
+        picture: { type: String },
+        token: { type: String, select: false },
+    },
+    {
+        versionKey: false,
+    }
+);
+
+userSchema.method('comparePassword', async function (password = '') {
+    return await bcrypt.compare(password, this.password);
 });
+
+userSchema.method('syncPicture', async function (url: string) {
+    this.picture = url;
+    await this.save();
+});
+
+userSchema.method('generateToken', async function () {
+    this.token = crypto.randomBytes(32).toString('hex');
+    await this.save();
+
+    return this.token;
+});
+
+userSchema.method('hash', async function (password) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(password, salt);
+    this.save();
+
+    return this.password;
+});
+
+userSchema.method('toJSON', function () {
+    return {
+        id: this._id,
+        username: this.username,
+        email: this.email,
+        picture: this.picture,
+    };
+});
+
+(userSchema.query as any).simple = function () {
+    return this.select(['username', 'email']);
+};
 
 export default mongoose.model<UserDocument>('User', userSchema);
