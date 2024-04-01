77b7f484e924b8556ce52b7d9a249324665eef17
Azka <falcxxdev@pm.me>
2024-04-01 20:28:37 +0000

    fix(rm): remove unecessary function

--- functions/useYTData.ts
+++ functions/useYTData.ts
@@ -6,23 +6,27 @@ export const useYTData = async (handle: string, useId?: boolean) => {
     if (useId) url = `https://youtube.com/channel/${handle}`;
     const data = await useChannelGetter(url);
     const about = await useChannelAbout(url);
-    const datahead = data.header?.as(YTNodes.C4TabbedHeader);
-    const abouthead = about?.as(YTNodes.AboutChannel);
-    const filterred = {
-        picture: data?.metadata.avatar,
-        banner: datahead?.banner,
-        subscribers: datahead?.subscribers?.text,
-        videos: datahead?.videos_count?.text,
-        is_verified: datahead?.author.is_verified,
-        is_membership: !datahead?.sponsor_button?.is_disabled,
-    };
-    const filterredAbout = {
-        views_count: abouthead.metadata?.view_count,
-        joined_date: abouthead.metadata?.joined_date?.text,
-        country: abouthead.metadata?.country,
-        description: abouthead.metadata?.description,
-        links: abouthead.metadata?.links,
-    };
+    try {
+        const datahead = data.header?.as(YTNodes.C4TabbedHeader);
+        const abouthead = about?.as(YTNodes.AboutChannel);
+        const filterred = {
+            picture: data?.metadata.avatar,
+            banner: datahead?.banner,
+            subscribers: datahead?.subscribers?.text,
+            videos: datahead?.videos_count?.text,
+            is_verified: datahead?.author.is_verified,
+            is_membership: !datahead?.sponsor_button?.is_disabled,
+        };
+        const filterredAbout = {
+            views_count: abouthead.metadata?.view_count,
+            joined_date: abouthead.metadata?.joined_date?.text,
+            country: abouthead.metadata?.country,
+            description: abouthead.metadata?.description,
+            links: abouthead.metadata?.links,
+        };
 
-    return { ...filterred, ...filterredAbout };
+        return { ...filterred, ...filterredAbout };
+    } catch (err) {
+        console.log(err);
+    }
 };
