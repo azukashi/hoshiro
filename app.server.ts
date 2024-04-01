980477c0b351dec098e927a2c88a8d344f908437
Azka <falcxxdev@pm.me>
2024-04-01 20:28:09 +0000

    chore(mongoose): enable autoIndex

--- app.server.ts
+++ app.server.ts
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 if (!process.env.DATABASE_URI) throw new Error('DATABASE_URI is required! Please check your .env file');
 if (!process.env.SECRET_SESSION) throw new Error('SECRET_SESSION is required! Please check your .env file');
 
-mongoose.connect(process.env.DATABASE_URI as string, { retryWrites: true }).then(() => {
+mongoose.connect(process.env.DATABASE_URI as string, { retryWrites: true, autoIndex: true }).then(() => {
     console.log('[INFO] App connected to MongoDB Atlas!');
     app.listen(port, () => {
         console.log(`[INFO] Express server has been started! (Live at port ${port})`);
