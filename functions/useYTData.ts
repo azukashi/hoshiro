7c33d70cb7a24c42578f31b67be275245484887c
Initial A <37253794+azkaafx@users.noreply.github.com>
2024-05-03 08:26:48 +0000

    fix(func): adaptation regarding to previous commit

--- functions/useYTData.ts
+++ functions/useYTData.ts
@@ -1,21 +1,20 @@
-import { useChannelGetter, useChannelAbout } from './useChannel';
+import { useChannelGetter } from './useChannel';
 import { YTNodes } from 'youtubei.js';
 
 export const useYTData = async (handle: string, useId?: boolean) => {
     let url = `https://youtube.com/${handle}`;
     if (useId) url = `https://youtube.com/channel/${handle}`;
-    const data = await useChannelGetter(url);
-    const about = await useChannelAbout(url);
+    const { channel, about } = await useChannelGetter(url);
     try {
-        if (data.header?.is(YTNodes.C4TabbedHeader)) {
+        if (channel.header?.is(YTNodes.C4TabbedHeader)) {
             const abouthead = about?.as(YTNodes.AboutChannel);
             const filterred = {
-                picture: data?.metadata.avatar,
-                banner: data?.header?.banner,
-                subscribers: data?.header?.subscribers?.text,
-                videos: data?.header?.videos_count?.text,
-                is_verified: data?.header?.author.is_verified,
-                is_membership: !data?.header?.sponsor_button?.is_disabled,
+                picture: channel?.metadata.avatar,
+                banner: channel?.header?.banner,
+                subscribers: channel?.header?.subscribers?.text,
+                videos: channel?.header?.videos_count?.text,
+                is_verified: channel?.header?.author.is_verified,
+                is_membership: !channel?.header?.sponsor_button?.is_disabled,
             };
             const filterredAbout = {
                 views_count: abouthead.metadata?.view_count,
