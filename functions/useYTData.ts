9e276de66b27c63569ef590747ed470d72702f8a
Azka <falcxxdev@pm.me>
2024-05-11 13:07:01 +0700

    fix(useYTData): handle PageHeader channel header
    Regarding to https://github.com/LuanRT/YouTube.js/issues/659, YouTube is slowly migrating to the PageHeader instead of C4TabbedHeader. This commit fixed the issue where YouTube doesn't return data in C4TabbedHeader form

--- functions/useYTData.ts
+++ functions/useYTData.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { useChannelGetter } from './useChannel';
 import { YTNodes } from 'youtubei.js';
 
@@ -13,8 +14,29 @@ export const useYTData = async (handle: string, useId?: boolean) => {
                 banner: channel?.header?.banner,
                 subscribers: channel?.header?.subscribers?.text,
                 videos: channel?.header?.videos_count?.text,
-                is_verified: channel?.header?.author.is_verified,
-                is_membership: !channel?.header?.sponsor_button?.is_disabled,
+                is_verified: channel?.header?.author.is_verified ? true : false,
+                is_membership: channel?.header?.sponsor_button ? true : false,
+                type: 'C4TabbedHeader',
+            };
+            const filterredAbout = {
+                views_count: abouthead.metadata?.view_count,
+                joined_date: abouthead.metadata?.joined_date?.text,
+                country: abouthead.metadata?.country,
+                description: abouthead.metadata?.description,
+                links: abouthead.metadata?.links,
+            };
+
+            return { ...filterred, ...filterredAbout };
+        } else if (channel.header?.is(YTNodes.PageHeader)) {
+            const abouthead = about?.as(YTNodes.AboutChannel);
+            const filterred = {
+                picture: channel?.metadata.avatar,
+                banner: channel.header.content?.banner?.image,
+                subscribers: channel?.header?.content?.metadata?.metadata_rows[1]?.metadata_parts[0].text.text,
+                videos: channel?.header?.content?.metadata?.metadata_rows[1].metadata_parts[1].text.text,
+                is_verified: channel?.header?.content?.title?.text.runs[0].attachment ? true : false,
+                is_membership: channel?.header.content?.actions?.actions_rows[0].actions[1] ? true : false,
+                type: 'PageHeader',
             };
             const filterredAbout = {
                 views_count: abouthead.metadata?.view_count,
@@ -26,7 +48,7 @@ export const useYTData = async (handle: string, useId?: boolean) => {
 
             return { ...filterred, ...filterredAbout };
         } else {
-            throw new Error('Header is not C4TabbedHeader');
+            throw new Error('Header is not one of C4TabbedHeader or PageHeader');
         }
         // const datahead = data.header?.as(YTNodes.C4TabbedHeader);
         // const abouthead = about?.as(YTNodes.AboutChannel);
